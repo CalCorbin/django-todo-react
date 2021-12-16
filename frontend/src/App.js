bfe1120d6d0701c046d79cc631ec669db0fcda04
Cal Corbin <ccorbin@remmac06562.local>
2021-12-16 11:30:47 -0600

    Refactor tab display

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,83 +1,89 @@
-import React, { Component } from "react";
-import Modal from "./components/Modal";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+// import Modal from "./components/Modal";
+// import axios from "axios";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      viewCompleted: false,
-      todoList: [],
-      modal: false,
-      activeItem: {
-        title: "",
-        description: "",
-        completed: false,
-      },
+function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [displayCompleted, setDisplayCompleted] = useState(false);
+  // const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    let mounted = true;
+
+    const fetchData = async () => {
+      const response = await fetch("/api/todos/");
+      return response.json();
     };
-  }
 
-  componentDidMount() {
-    this.refreshList();
-  }
+    fetchData().then((data) => {
+      if (!mounted) return;
 
-  refreshList = () => {
-    axios
-      .get("/api/todos/")
-      .then((res) => this.setState({ todoList: res.data }))
-      .catch((err) => console.log(err));
+      setTodoList(data);
+    });
+
+    return () => {
+      mounted = false;
+    };
+  }, []);
+
+  // const refreshList = () => {
+  //   axios
+  //     .get("/api/todos/")
+  //     .then((res) => this.setState({ todoList: res.data }))
+  //     .catch((err) => console.log(err));
+  // };
+
+  // const toggle = () => {
+  //   this.setState({ modal: !this.state.modal });
+  // };
+  //
+  // const handleSubmit = (item) => {
+  //   this.toggle();
+  //
+  //   if (item.id) {
+  //     axios
+  //       .put(`/api/todos/${item.id}/`, item)
+  //       .then((res) => this.refreshList());
+  //     return;
+  //   }
+  //   axios.post("/api/todos/", item).then((res) => this.refreshList());
+  // };
+  //
+  // const handleDelete = (item) => {
+  //   axios.delete(`/api/todos/${item.id}/`).then((res) => this.refreshList());
+  // };
+  //
+  const createItem = () => {
+    console.log("createItem");
+    // const item = { title: "", description: "", completed: false };
+
+    // this.setState({ activeItem: item, modal: !this.state.modal });
   };
 
-  toggle = () => {
-    this.setState({ modal: !this.state.modal });
-  };
+  // const editItem = (item) => {
+  //   this.setState({ activeItem: item, modal: !this.state.modal });
+  // };
+  //
+  // const toggleTabs = (status) => {
+  //   // if (status) {
+  //   // return this.setState({ viewCompleted: true });
+  //   // }
+  //   // return this.setState({ viewCompleted: false });
+  //   return setDisplayCompleted(status);
+  // };
 
-  handleSubmit = (item) => {
-    this.toggle();
-
-    if (item.id) {
-      axios
-        .put(`/api/todos/${item.id}/`, item)
-        .then((res) => this.refreshList());
-      return;
-    }
-    axios.post("/api/todos/", item).then((res) => this.refreshList());
-  };
-
-  handleDelete = (item) => {
-    axios.delete(`/api/todos/${item.id}/`).then((res) => this.refreshList());
-  };
-
-  createItem = () => {
-    const item = { title: "", description: "", completed: false };
-
-    this.setState({ activeItem: item, modal: !this.state.modal });
-  };
-
-  editItem = (item) => {
-    this.setState({ activeItem: item, modal: !this.state.modal });
-  };
-
-  displayCompleted = (status) => {
-    if (status) {
-      return this.setState({ viewCompleted: true });
-    }
-
-    return this.setState({ viewCompleted: false });
-  };
-
-  renderTabList = () => {
+  const renderTabList = () => {
     return (
       <div className="nav nav-tabs">
         <span
-          onClick={() => this.displayCompleted(true)}
-          className={this.state.viewCompleted ? "nav-link active" : "nav-link"}
+          onClick={() => setDisplayCompleted(true)}
+          className={displayCompleted ? "nav-link" : "nav-link"}
         >
           Complete
         </span>
         <span
-          onClick={() => this.displayCompleted(false)}
-          className={this.state.viewCompleted ? "nav-link" : "nav-link active"}
+          onClick={() => setDisplayCompleted(false)}
+          className={displayCompleted ? "nav-link" : "nav-link active"}
         >
           Incomplete
         </span>
@@ -85,72 +91,66 @@ class App extends Component {
     );
   };
 
-  renderItems = () => {
-    const { viewCompleted } = this.state;
-    const newItems = this.state.todoList.filter(
-      (item) => item.completed === viewCompleted
-    );
+  // const renderItems = () => {
+  //   const { viewCompleted } = this.state;
+  //   const newItems = this.state.todoList.filter(
+  //     (item) => item.completed === viewCompleted
+  //   );
+  //
+  //   return newItems.map((item) => (
+  //     <li
+  //       key={item.id}
+  //       className="list-group-item d-flex justify-content-between align-items-center"
+  //     >
+  //       <span
+  //         className={`todo-title mr-2 ${
+  //           this.state.viewCompleted ? "completed-todo" : ""
+  //         }`}
+  //         title={item.description}
+  //       >
+  //         {item.title}
+  //       </span>
+  //       <span>
+  //         <button
+  //           className="btn btn-secondary mr-2"
+  //           onClick={() => this.editItem(item)}
+  //         >
+  //           Edit
+  //         </button>
+  //         <button
+  //           className="btn btn-danger"
+  //           onClick={() => this.handleDelete(item)}
+  //         >
+  //           Delete
+  //         </button>
+  //       </span>
+  //     </li>
+  //   ));
+  // };
 
-    return newItems.map((item) => (
-      <li
-        key={item.id}
-        className="list-group-item d-flex justify-content-between align-items-center"
-      >
-        <span
-          className={`todo-title mr-2 ${
-            this.state.viewCompleted ? "completed-todo" : ""
-          }`}
-          title={item.description}
-        >
-          {item.title}
-        </span>
-        <span>
-          <button
-            className="btn btn-secondary mr-2"
-            onClick={() => this.editItem(item)}
-          >
-            Edit
-          </button>
-          <button
-            className="btn btn-danger"
-            onClick={() => this.handleDelete(item)}
-          >
-            Delete
-          </button>
-        </span>
-      </li>
-    ));
-  };
-
-  render() {
-    return (
-      <main className="container">
-        <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
-        <div className="row">
-          <div className="col-md-6 col-sm-10 mx-auto p-0">
-            <div className="card p-3">
-              <div className="mb-4">
-                <button className="btn btn-primary" onClick={this.createItem}>
-                  Add task
-                </button>
-              </div>
-              {this.renderTabList()}
-              <ul className="list-group list-group-flush border-top-0">
-                {this.renderItems()}
-              </ul>
+  console.log("todoList", todoList);
+  return (
+    <main className="container">
+      <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
+      <div className="row">
+        <div className="col-md-6 col-sm-10 mx-auto p-0">
+          <div className="card p-3">
+            <div className="mb-4">
+              <button className="btn btn-primary" onClick={createItem}>
+                Add task
+              </button>
             </div>
+            {renderTabList()}
+            <ul className="list-group list-group-flush border-top-0">
+              {todoList.map((item) => (
+                <li key={item.id}>{item.title}</li>
+              ))}
+            </ul>
           </div>
         </div>
-        {this.state.modal ? (
-          <Modal
-            activeItem={this.state.activeItem}
-            toggle={this.toggle}
-            onSave={this.handleSubmit}
-          />
-        ) : null}
-      </main>
-    );
-  }
+      </div>
+    </main>
+  );
 }
 
 export default App;
